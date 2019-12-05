a084833a2e9161fa32d84f39e88b4ccb079a962c
AndrewBrin <Andrew_live2@hotmail.com>
2019-12-06 08:04:42 +1100

    No changes

---
+++ TestEmail.php
@@ -0,0 +1,17 @@
+//Test for Heroku deployment and git
+
+<?php
+
+$to = 'andrew_live2@hotmail.com';
+
+$subject = 'this is our test email';
+
+$message = 'hi there, thanks for testing';
+
+
+mail($to,$subject,$message);
+
+
+?>
+
+//<h1> <?php echo "hello world";?></h1>
